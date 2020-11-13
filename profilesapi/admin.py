c412ed8c73379859c717a68d6dae57210c5e1119
BaDnja <badnjevic.amar1@gmail.com>
2020-11-13 19:33:04 +0100

    Added users profile feed api

--- profilesapi/admin.py
+++ profilesapi/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, ProfileFeedItem
 
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProfileFeedItem)
+class ProfileFeedItemAdmin(admin.ModelAdmin):
     pass
